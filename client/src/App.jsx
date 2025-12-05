54cffb620c8730ec3dd2449210d8c2e2e6b758de
Zaphod1977 <carfiguru@hotmail.com>
2025-12-05 16:09:28 -0600

    added ownership labeling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import CategoriesList from './pages/CategoriesList';
 import CategoryPage from './pages/CategoryPage';
 import EntryPage from './pages/EntryPage';
-import SectionPage from './pages/SectionPage';   // ← ADD THIS LINE
+import SectionPage from './pages/SectionPage';
+import AdminLoginPage from './pages/AdminLoginPage';
 import { Container, Typography, Box, Button } from '@mui/material';
 import { Add } from '@mui/icons-material';
 
@@ -14,7 +15,18 @@ export default function App() {
           <Typography variant="h3" align="center" color="#0d47a1" fontWeight="bold" gutterBottom>
             OTWDS v2
           </Typography>
+          <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">
+            Bobby Bridges
+          </h1>
+          <div className="space-y-1 text-zinc-300 text-lg">
+            <p>802 West 6th St <br />
+              Staunton, IL 62088<br />
+              Phone: (314) 853-0016 <br />
+              Email: carfiguru@gmail.com</p>
+            <br />
+          </div>
           <CategoriesList />
+          {/* <AdminLoginPage/> */}
         </Container>
       } />
       <Route path="/category/:catId" element={<CategoryPage />} />
