790f50c9198f747e5f62220fbd25f07641ed5b5a
Zaphod1977 <carfiguru@hotmail.com>
2025-12-23 02:08:55 -0600

    backend redirect

--- client/src/api.js
+++ client/src/api.js
@@ -1,12 +1,10 @@
-// client/src/api.js  ← ONE FILE TO RULE THEM ALL
-
 import axios from 'axios';
 
 // CHANGE THIS ONE LINE WHEN YOU GO LIVE
-// const API_BASE = 'https://otwds-api.onrender.com/api';
+const API_BASE = 'https://otwds-api.onrender.com/api';
 
 // For local testing
-const API_BASE = 'http://localhost:5000/api';
+// const API_BASE = 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE,
